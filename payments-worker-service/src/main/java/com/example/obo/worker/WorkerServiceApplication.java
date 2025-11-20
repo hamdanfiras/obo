f410b8b4889feeee856875b7c2b5b02c87d97e7c
Firas Hamdan <hamdanfiras@gmail.com>
2025-11-20 10:36:20 +0300

    Remove OAuth2ClientConfig and TokenExchangeService classes; update PaymentEventConsumer to include TokenExchangeService import and adjust component scanning in WorkerServiceApplication.

--- payments-worker-service/src/main/java/com/example/obo/worker/WorkerServiceApplication.java
+++ payments-worker-service/src/main/java/com/example/obo/worker/WorkerServiceApplication.java
@@ -5,9 +5,11 @@ import net.devh.boot.grpc.server.autoconfigure.GrpcServerFactoryAutoConfiguratio
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = { OAuth2ResourceServerAutoConfiguration.class, GrpcServerAutoConfiguration.class,
         GrpcServerFactoryAutoConfiguration.class })
+@ComponentScan(basePackages = { "com.example.obo.worker", "com.example.obo.common" })
 public class WorkerServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(WorkerServiceApplication.class, args);
